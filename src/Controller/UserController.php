2922805a4227dd7b8c3a8ab9f83a47db6c9ae7ea
SenpaiTn <khaledsahli36@gmail.com>
2021-08-14 14:43:50 +0100

    initial import

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,6 +14,8 @@ class UserController extends AbstractController
      */
     public function index(): Response
     {
-        return new Response('hello');
+        $em = $this->getDoctrine()->getManager();
+        $users = $em->getRepository(User::class)->findAll();
+        return new Response(count($users) . ' users');
     }
 }
