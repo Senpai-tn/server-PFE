25149da7930b2c186f699988cffb19312eae1301
SenpaiTn <khaledsahli36@gmail.com>
2021-08-14 14:53:40 +0100

    initial import

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,14 +13,16 @@ class UserController extends AbstractController
     /**
      * @Route("/", name="user")
      */
-    public function index(): Response
+    public function index(Request $r): Response
     {
+        $data = json_decode($r->getContent(), true);
+
         $em = $this->getDoctrine()->getManager();
         $user = new User();
         $user->setFirstName('Khaled');
         $em->persist($user);
         $em->flush();
         $users = $em->getRepository(User::class)->findAll();
-        return new Response(count($users) . ' users');
+        return new Response(count($users) . ' users' . $data['id']);
     }
 }
